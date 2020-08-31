1c497b90962dbd7cbbc83037883cadaf16f807a6
Erilyx <erilyx@gmail.com>
2020-08-31 11:10:48 -0400

    Fixed movement and jumping
    removed all add force and just used velocities.... WAY easier.

--- Assets/JenkinsController.cs
+++ Assets/JenkinsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class JenkinsController : MonoBehaviour
@@ -7,13 +8,22 @@ public class JenkinsController : MonoBehaviour
     private Rigidbody2D jenkinsRigidBody2D;
     
     [Header("Default Movement")]
-    public float scrollSpeed = 10f;
-    private Vector3 goJenkinsGo = new Vector3(1, 0, 0);
+    public float scrollSpeed = 4f;
     public float linearDrag = 4f;
     public LayerMask groundLayer;
     public bool onGround;
-    public float groundLength = 0.6f;
-    public float jumpSpeed = 15f;
+    public bool onRightWall = false;
+    public bool onLeftWall = false;
+    private float groundLength = 0.7f;
+
+    public float gravity = 1f;
+    public float fallMultiplier = 5f;
+
+    [Header("Jump Abilities")]
+    private float jumpPowerX = 1f;
+    public float jumpPowerY = 15f;
+    public bool canJump = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,38 +35,69 @@ public class JenkinsController : MonoBehaviour
     void Update()
     {
         onGround = Physics2D.Raycast(transform.position, Vector2.down, groundLength, groundLayer);
+        onRightWall = Physics2D.Raycast(transform.position, Vector2.right, groundLength, groundLayer);
+        onLeftWall = Physics2D.Raycast(transform.position, Vector2.left, groundLength, groundLayer);
 
-        if (((Input.GetKeyDown(KeyCode.X)) || (Input.GetKeyDown(KeyCode.Space))) && onGround)
+        ScrollJenkins();
+        
+
+        ModifyPhysics();  
+
+        // jump
+        if (Input.GetKey(KeyCode.X) && onGround && canJump)
         {
             Jump();
+            canJump = false;
         }
-    }
+
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            canJump = true;
+        }
+    }//end of Update
 
     private void FixedUpdate()
     {
-        if(onGround)
-        {
-            ScrollJenkins();
-        }
-        ModifyPhysics();
-    }
+
+    }//end of Fixed Update
+
 
     void ScrollJenkins()
     {
-        jenkinsRigidBody2D.AddForce(goJenkinsGo, ForceMode2D.Impulse);   //applies a lot of force each time
-        if(jenkinsRigidBody2D.velocity.magnitude > scrollSpeed)          //is he going to fast?
+        if (!onRightWall && !onLeftWall)
         {
-            jenkinsRigidBody2D.velocity = Vector3.ClampMagnitude(jenkinsRigidBody2D.velocity, scrollSpeed);  //limits him to scrollSpeed
+             jenkinsRigidBody2D.velocity = new Vector2(scrollSpeed, jenkinsRigidBody2D.velocity.y);
         }
     }
 
     void Jump()
     {
-        jenkinsRigidBody2D.velocity = new Vector2(jenkinsRigidBody2D.velocity.x, 0);
-        jenkinsRigidBody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        Debug.Log("is in the jump function");
+        jenkinsRigidBody2D.velocity = new Vector2(jenkinsRigidBody2D.velocity.x, jumpPowerY);
+
     }
+
     void ModifyPhysics()
     {
+
+        if (onGround)
+        {
+            jenkinsRigidBody2D.gravityScale = 0;
+        }
+        else
+        {
+            jenkinsRigidBody2D.gravityScale = gravity;
+            jenkinsRigidBody2D.drag = linearDrag * 0.15f;
+            if(jenkinsRigidBody2D.velocity.y < 0)  //if is coming down
+            {
+                jenkinsRigidBody2D.gravityScale = gravity * fallMultiplier;
+            }
+            else if((jenkinsRigidBody2D.velocity.y > 0) && !Input.GetKey(KeyCode.X))
+            {
+                jenkinsRigidBody2D.gravityScale = gravity * (fallMultiplier / 2);
+            }
+        }
 
     }
 
@@ -64,5 +105,7 @@ public class JenkinsController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundLength);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * groundLength);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.left * groundLength);
     }
 }
